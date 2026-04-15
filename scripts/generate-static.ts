ea565ac804807c3f53d4462174aa31c7b9680905
zenhogar2050-byte <zenhogar2050@gmail.com>
2026-04-15 17:31:52 -0500

    instalando tag de google

--- scripts/generate-static.ts
+++ scripts/generate-static.ts
@@ -16,6 +16,15 @@ const template = (title: string, description: string, canonical: string, content
 <!DOCTYPE html>
 <html lang="es">
 <head>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-57BY2PVKF4"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-57BY2PVKF4');
+    </script>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${title} | Zenhogar</title>
